5d865d6e37d91494c91fadebfa8fc2dab3b498de
Kedra, Krystian <krystian.kedra@atos.net>
2019-04-08 14:36:33 +0200

    Changed logic to search for title and description

--- api/filters/filters.ts
+++ api/filters/filters.ts
@@ -2,5 +2,5 @@ import { BookInterface } from "~/types";
 
 export const filterById = (value: BookInterface['id']) => (item: BookInterface) => item.id !== value;
 export const filterByValue = (value: string) => (item: BookInterface) =>
-    item.title.toLocaleLowerCase().startsWith(value.toLocaleLowerCase())
-    || item.description.toLocaleLowerCase().startsWith(value.toLocaleLowerCase());
+    item.title.toLocaleLowerCase().includes(value.toLocaleLowerCase())
+    || item.description.toLocaleLowerCase().includes(value.toLocaleLowerCase());
