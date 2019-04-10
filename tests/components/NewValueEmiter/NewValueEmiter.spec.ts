e74dd12b19bb266ce87f1828e01fa837108676bf
Kedra, Krystian <krystian.kedra@atos.net>
2019-04-10 18:55:04 +0200

    Updated newValueEmiter test

--- tests/components/NewValueEmiter/NewValueEmiter.spec.ts
+++ tests/components/NewValueEmiter/NewValueEmiter.spec.ts
@@ -1,31 +1,27 @@
-import { mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import NewValueEmiter from '~/components/NewValueEmiter/NewValueEmiter.vue';
 
 describe('NewValueEmiter', () => {
-
+    const wrapper = shallowMount(NewValueEmiter, {
+        propsData: {
+            newValue: 'loginValue',
+            itemHeader: 'Login'
+        }
+    });
     test('Check props from parent', () => {
-        const wrapper = mount(NewValueEmiter, {
-            propsData: {
-                newValue: 'loginValue',
-                itemHeader: 'Login'
-            },
-            computed: {
-                updateValue: {
-                    get(): string {
-                        return this.newValue;
-                    },
-                    set (value: string) {
-                        this.$emit('update:newValue', value);
-                    }
-                }
-            }
-        });
         expect(wrapper.props().newValue).toBe('loginValue');
         expect(wrapper.props().itemHeader).toBe('Login');
     });
 
+    test('isExist', () => {
+        expect(wrapper.exists()).toBe(true)
+        expect(wrapper.find('input').exists()).toBe(true);
+        expect(wrapper.find('.form-control').exists()).toBe(true);
+        expect(wrapper.is(NewValueEmiter)).toBe(true);
+    });
+
     test('Find and Set Value', () => {
-        const wrapper = mount(NewValueEmiter);
+        const wrapper = shallowMount(NewValueEmiter);
         const input = wrapper.find('.form-control');
         input.setValue('KrystianTesting');
     });
